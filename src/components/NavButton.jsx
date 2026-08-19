a400487db41c084eb63910b14135007c36ee4fe4
BorisJakubik <jakubik@yahoo.com>
2026-08-19 22:52:17 +0200

    Add src/components/NavButton.jsx

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const NavButton = ({ icon: Icon, label, active, onClick }) => (
+  <button className={active ? 'nav-active' : ''} onClick={onClick}>
+    <Icon size={21} />
+    <span>{label}</span>
+  </button>
+);
